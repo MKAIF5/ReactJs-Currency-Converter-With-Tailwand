bcf65aee87d50a717aa2ca34de9285ac9c826ed0
mohammed kaif <mkaif555123@gmail.com>
2024-09-02 00:33:06 +0500

    make tommorow today postponed

--- Currency-Converter/src/App.jsx
+++ Currency-Converter/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
-
+      <h1>Currency Converter</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
