71dee22e2adbee8ace6f75f4de2dc9fee07306ba
mohammed kaif <mkaif555123@gmail.com>
2024-09-02 20:39:24 +0500

    35% Work Completed

--- Currency-Converter/src/components/InputBox.jsx
+++ Currency-Converter/src/components/InputBox.jsx
@@ -1,5 +1,14 @@
-function InputBox({ label, className = "" }) {
-
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) {
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
@@ -18,12 +27,8 @@ function InputBox({ label, className = "" }) {
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select
-          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-        >
-            <option value="usd">
-                usd
-            </option>
+        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
+          <option value="usd">usd</option>
         </select>
       </div>
     </div>
