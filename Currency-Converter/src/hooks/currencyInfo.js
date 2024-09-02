ca1913364117ae8ec7052c9462e311da78656ddb
mohammed kaif <mkaif555123@gmail.com>
2024-09-02 18:50:53 +0500

    5% Work Completed

---
+++ Currency-Converter/src/hooks/currencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function currencyinfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest
+            /currencies/${currency}.json`)
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((resp) => {
+                return setData(resp[currency])
+            })
+        console.log(data);
+    }, [currency])
+}
